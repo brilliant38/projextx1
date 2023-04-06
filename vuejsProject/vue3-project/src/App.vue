043efdab1b0da8af65895e2a043a449be6c0ca92
KwangJae Lee <lkg3796@email.com>
2023-04-06 11:48:52 +0900

    프로젝트로 배우는 Vue.js 3 00 소개 [Vue.js 3 기초 익히기 뷰js 3 강의 강좌 튜토리얼 입문 초보] 01 설치하기 [Vue.js 3 기초 익히기 뷰js 3 강의 강좌 튜토리얼] 02 폴더구조 [Vue.js 3 기초 익히기 뷰js 3 강의 강좌 튜토리얼] 03 컴포넌트 [Vue.js 3 기초 익히기 뷰js 3 강의 강좌 튜토리얼] 04 fragment [Vue.js 3 기초 익히기 뷰js 3 강의 강좌 튜토리얼] 05 함수 [Vue.js 3 기초 익히기 뷰js 3 강의 강좌 튜토리얼]

---
+++ vuejsProject/vue3-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- return 시킨 변수는 {{ 이 안에 넣음 }}-->
+  <div class="name">
+    {{greet}}
+  </div>
+</template>
+
+<script>
+export default{
+  //Composition API만 사용한다고 함.
+  //사용할 함수 정의
+  setup(){
+    const name = 'Manlu coder1';
+
+    const greeting = (name) =>{
+      return 'Hell O, ' + name;
+    };
+    const greet = greeting(name);
+
+    return {
+      greet,
+    }
+  }
+}
+</script>
+
+<style>
+ .name {
+  color:red;
+}
+</style>

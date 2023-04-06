46528ea4b13cbe55f281b23d419d92aa0bc4cb29
KwangJae Lee <lkg3796@email.com>
2023-04-06 13:29:41 +0900

    프로젝트로 배우는 vue.js3 06 event [Vue.js 3 기초 익히기 뷰js 3 강의 강좌 튜토리얼] 07 ref & reactive [Vue.js 3 기초 익히기 뷰js 3 강의 강좌 튜토리얼] 유료강의 전환 문제로 중지

--- vuejsProject/vue3-project/src/App.vue
+++ vuejsProject/vue3-project/src/App.vue
@@ -1,24 +1,43 @@
 <template>
   <!-- return 시킨 변수는 {{ 이 안에 넣음 }}-->
   <div class="name">
-    {{greet}}
+    {{name}}
   </div>
+  <button 
+    class="btn btn-primary"
+    v-on:click="updateName"
+  >
+    Click
+    </button>
 </template>
 
 <script>
+import {ref} from 'vue';
 export default{
   //Composition API만 사용한다고 함.
   //사용할 함수 정의
-  setup(){
-    const name = 'Manlu coder1';
+  setup() {
+    const name = ref({
+      id : 1
+    });
 
-    const greeting = (name) =>{
+/*     const greeting = (name) =>{
       return 'Hell O, ' + name;
     };
-    const greet = greeting(name);
+
+    const greet = greeting(name); */
+
+    const updateName = () => {
+      //console.log('hello world');
+      //ref -> .value 수정
+      //reactive -> .key로 접근.
+      name.value = 2;
+      console.log(name);
+    };
 
     return {
-      greet,
+      name,
+      updateName,
     }
   }
 }

bf6c33b6f415c6fa9a34da0d4c323c5d27a276f1
KwangJae Lee <lkg3796@gmail.com>
2023-04-11 10:36:57 +0900

    C# 교과서 마스터하기
    02. 준비사항

---
+++ C#/HelloWithCoreWithVisualStudio/HelloWithCoreWithVisualStudio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWithCoreWithVisualStudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("안녕하세요.");
+        }
+    }
+}
---
+++ C#/HelloWithCoreWithVisualStudio2/HelloWithCoreWithVisualStudio2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWithCoreWithVisualStudio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

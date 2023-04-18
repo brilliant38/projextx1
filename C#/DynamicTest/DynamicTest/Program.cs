ac8c593edfd91ad4b1b7e6a905c3117a5ef2cec0
KwangJae Lee <lkg3796@gmail.com>
2023-04-19 08:16:16 +0900

    C# 교과서 마스터하기
    49. 확장 메서드 만들기
    50. 동적 형식

---
+++ C#/DynamicTest/DynamicTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicTest
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //런타임에 데이터 형식이 결정 되는 모든 형식을 담을 수 있는 특수한 유형의 형식
+            dynamic x;
+            x = 1_234;
+            Console.WriteLine($"{x} - {x.GetType()}");
+
+            x = "Dynamic Type!";
+            Console.WriteLine($"{x} - {x.GetType()}" );
+            //Console.WriteLine($"{x} - {x.xxxxxxx()}" );//런타임 시에 에러가 난다.
+
+            string ss = "Hello";
+            Console.WriteLine(ss);
+
+            var vs = "Hello";
+            Console.WriteLine(vs.Length);
+
+            dynamic ds = "Hello"; 
+            Console.WriteLine(ds.Length);
+
+            //vs와 dynamic은 동일하지만, 다이나믹 타입은 인텔리센스의 가이드 지원이 안된다.
+        }
+    }
+}
---
+++ C#/ExtendedMethodsTest/ExtendedMethodsTest/MyExtensionsDemo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyExtensions
+{
+    static class StringExtension
+    {
+        public static string Ten(this String msg) => msg.Substring(0, 10);
+        public static string Five(this String msg) => msg.Substring(0, 5);
+        public static string AddElipsis(this String msg) => msg + "...";
+        public static string AddElipsis(this String msg, string elipsis) => $"{msg}{elipsis}";
+    }
+    class MyExtensionsDemo
+    {
+        static void Main()
+        {
+            //확장 메서드 : 기존 형식에 메서드를 추가하는 기능으로
+            //확장 형식의 인스턴스 메서드로 사용
+            string message = "안녕하세요. 반갑습니다. 또 만나요.";
+            //10자리 자르고 다시 5자리 자름. 메서드 체이닝으로 결과에 대해 연속 실행.
+            Console.WriteLine(message.Ten().Five().AddElipsis()); 
+            //...을 입력 값 ___을 받아 바꿔서 출력
+            Console.WriteLine(message.Ten().Five().Five().AddElipsis("___")); 
+            Console.WriteLine(message.Five());
+
+        }
+    }
+}

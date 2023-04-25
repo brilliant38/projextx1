5ab35ee5893180e7463813a3ab8877068a31ecf7
KwangJae Lee <lkg3796@gmail.com>
2023-04-25 11:32:11 +0900

    C#, WPF
    1.HelloWorld(XAML기반)
    3.WPF 데이터바인딩 심플예제
    4. C# 코드기반 WPF HelloWorld

---
+++ C#/WpfApp1/WpfApp1/MyMain.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    class MyMain : Application
+    {
+        [STAThread]
+        public static void Main()
+        {
+            MyMain app = new MyMain();
+            //app.ShutdownMode = ShutdownMode.OnMainWindowClose; //Main이 꺼질때 같이 꺼짐
+            app.Run(); //화면에 안 보임.
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // main window
+            Window mainWindow = new Window();
+            mainWindow.Title = "WPF Sample(Main)";
+            mainWindow.MouseDown += WinMouseDown; //델리게이트
+            mainWindow.Show();
+
+            //sub windows
+            for (int i = 0; i < 2; i++)
+            {
+                Window win = new Window();
+                win.Title = "Extra Windows No." + (i + 1);
+                win.ShowInTaskbar = false;
+                win.Owner = mainWindow; //sub window를 main에 종속시킴
+                win.Show();
+            }
+        }
+
+        private void WinMouseDown(object sender, MouseEventArgs e)
+        {
+            Window win = new Window();
+            win.Title = "Modal DialogBox";
+            win.Width = 400;
+            win.Height = 200;
+
+            Button b = new Button();
+            b.Content = "Click Me!";
+            b.Click += Button_Click;
+            
+            win.Content = b;
+            win.ShowDialog(); //Modal 형태
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button Click!", sender.ToString());
+        }
+    }
+}
---
+++ C#/WpfOracleTest3/WpfOracleTest3/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using Oracle.DataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows;
+
+namespace WpfOracleTest3
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        OracleConnection conn;
+
+        private void DB_Connect(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string strCon = "data source=onj;User ID=scott;Password=tiger";
+                conn = new OracleConnection(strCon);
+                conn.Open();
+
+                MessageBox.Show("DB Connection OK!");
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.ToString());
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Select_Emp(object sender, RoutedEventArgs e)
+        {
+            string sql = "select empno, ename job from emp ";
+
+            OracleCommand comm = new OracleCommand();
+            if (conn == null) DB_Connect(this,null);
+            comm.Connection = conn;
+            comm.CommandText = sql;
+
+            OracleDataReader reader = comm.ExecuteReader(CommandBehavior.CloseConnection);
+            List<EmpViewModel> emps = new List<EmpViewModel>();
+            while (reader.Read())
+            {
+                emps.Add(new EmpViewModel()
+                {
+                    Empno = reader.GetInt32(reader.GetOrdinal("empno")),
+                    Ename = reader.GetString(reader.GetOrdinal("ename")),
+                    Job = reader.GetString(reader.GetOrdinal("job"))
+                });
+            }
+
+            IstView.ItemsSource = emps;
+            reader.Close();
+
+        }
+    }
+}

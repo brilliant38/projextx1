b7987a44d2c85ab80d21eea6d5e4d81e6c1693ea
KwangJae Lee <lkg3796@gmail.com>
2023-04-26 16:22:10 +0900

    WPF동영상강좌
    6. 프로퍼티트리거
    7. WPF Trigger 데이터 트리거
    8. ListBox와 LINQ쿼리데이터바인딩, 새창띄우기, 이벤트 및 델리게이트를 통한 메인 윈도우리스트박스리프레쉬
    9. 의존프로퍼티(DependencyProperty), 의존속성
    10.WPF이벤트라우팅(RoutedEvent), 버블링(Bubbling), 터널링(Tunneling) 이벤트

---
+++ C#/EventRoutingWPF/EventRoutingWPF/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EventRoutingWPF
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string mouseActivity = string.Empty;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //버블링 이벤트 -> button - stackpanel - windows 순으로 상위 진행 됨
+        //private void Window_Click(object sender, RoutedEventArgs e)
+        //{
+        //    txt3.Text = "Click event is bubbled to Window";
+        //}
+
+        //private void StackPanel_Click(object sender, RoutedEventArgs e)
+        //{
+        //    txt2.Text = "Click event is bubbled to Stack Panel";
+        //}
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    txt1.Text = "Button is Clicked";
+        //    e.Handled = true; // 상위 객체로 버블링 되는것을 막음
+        //}
+
+        //터널링 이벤트 -> windows - stackpanel - button 순으로 하위 진행 됨
+        private void Window_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            mouseActivity = "PreviewMouseDown Window \n";
+            MessageBox.Show(mouseActivity);
+        }
+
+        private void StackPanel_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            mouseActivity = "PreviewMouseDown StackPanel \n";
+            MessageBox.Show(mouseActivity);
+        }
+
+        private void Button_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            mouseActivity = "PreviewMouseDown Button \n";
+            MessageBox.Show(mouseActivity);
+        }
+    }
+}
---
+++ C#/WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //DependencyProperty(MyProperty)를 위한 래퍼속성 MyColor
+        public String MyColor
+        {
+            get { return (String)GetValue(MyProperty); }
+            set { SetValue(MyProperty, value); }
+        }
+
+        //의존속성 정의
+        public static readonly DependencyProperty MyProperty = DependencyProperty.Register(
+            "MyColor",           //의존속성으로 등록 될 속성
+            typeof(String),
+            typeof(MainWindow),
+            new FrameworkPropertyMetadata(new PropertyChangedCallback(OnMyPropertyChanged)
+            ));
+
+        private static void OnMyPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            MainWindow win = d as MainWindow;
+            SolidColorBrush brush = (SolidColorBrush)new BrushConverter().ConvertFromString(e.NewValue.ToString());
+            win.Background = brush;
+            win.Title = (e.OldValue == null) ? "이전배경색 없음" : "배경색:" + e.OldValue.ToString();
+            win.textBox1.Text = e.NewValue.ToString();
+        }
+
+        private void ContextMenu_Click(object sender, RoutedEventArgs e)
+        {
+            string str = (e.Source as MenuItem).Header as string;
+            MyColor = str;
+        }
+    }
+}

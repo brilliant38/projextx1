c77e23fe98221e0e299e15a444042e0c021ed5bc
KwangJae Lee <lkg3796@gmail.com>
2023-04-26 17:21:56 +0900

    WPF동영상강좌
    11.데이터바인딩(DataBinding)이란?

--- C#/DataBindingWPF/DataBindingWPF/MainWindow.xaml.cs
+++ C#/DataBindingWPF/DataBindingWPF/MainWindow.xaml.cs
@@ -20,9 +20,35 @@ namespace DataBindingWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        ////바인딩 대상 속성들
+        //public bool Seoul { get; set; }
+        //public bool Jeju { get; set; }
+        //public bool Incheon { get; set; }
+
         public MainWindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+
+            //바인딩의 소스객체, UI컨트롤에서 소스 지정없이 사용가능.
+            //window의 하위 객체에서 소스 속성으로 사용가능.
+            //this.DataContext = this;
+
+            //MyViewModel _viewmodel = new MyViewModel()
+            //{
+            //    Date = new DateTime(2023, 4, 26),
+            //    Title = "WPF User Group"
+            //};
+            //this.DataContext = _viewmodel;
+
         }
+
+
+        //버튼 클릭 이벤트 핸들러
+        //private void Summit_Click(object sender, RoutedEventArgs e)
+        //{
+        //    MessageBox.Show(string.Format("SEOUL: {0}, JEJU: {1}, INCHEON: {2}", Seoul, Jeju, Incheon));
+        //}
+
+
     }
 }

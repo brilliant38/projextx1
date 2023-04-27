a6bc4d658c4eabade3915dd6a68846058655ef3b
KwangJae Lee <lkg3796@gmail.com>
2023-04-27 09:51:24 +0900

    WPF동영상강좌
    12.데이터바인딩 실습 동영상
    13.C#, Wpf DataContext를 이용한 데이터바인딩 실습
    14.INotyfyPropertChanged, PropertyChanged 이용

---
+++ C#/DataContextBindingWPF/DataContextBindingWPF/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataContextBindingWPF
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Emp e = new Emp()
+            {
+                Ename = "홍길동",
+                City = "Seoul"
+
+            };
+
+            this.DataContext = e;
+        }
+
+        private void Oncliked(object sender, RoutedEventArgs args)
+        {
+            Emp e = this.DataContext as Emp;
+            Console.WriteLine(e.Ename);
+            Console.WriteLine(e.City);
+        }
+    }
+}
---
+++ C#/WpfApp3/WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Binding bind = new Binding();
+            //bind.Source = txt1;
+            //bind.Path = new PropertyPath(TextBox.TextProperty);
+            //SetBinding의 첫 번째 인자는 DependencyProperty 타입이 되어야 한다.
+            //그러므로 타겟의 속성은 의존 속성의 지원을 받는 속성이 되야한다.
+            //label.SetBinding(Label.ContentProperty, bind);
+        }
+    }
+}
